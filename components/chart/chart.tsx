dce5347f81c698c0e9984d843c7040c78e3e4106
Michelle Penna <mp.design.apps@gmail.com>
2023-03-28 18:11:34 -0500

    feat(Chart): get data json in chart

--- components/chart/chart.tsx
+++ components/chart/chart.tsx
@@ -1,26 +1,32 @@
 import data from "../../json/data.json";
 
-export interface IDataChart {
-  day?: string;
-  amount?: number;
-}
-
-const Chart = ({ day, amount }: IDataChart) => {
-  const columns = data.data[0];
+const Chart = () => {
+  const columns = data.data;
 
   return (
-    <section className="chart">
+    <section className="resume">
       <h3>Spending - Last 7 days</h3>
-      {/* <ul>
-        {columns.map(() => {
-          <li>
-            <div />
-            <caption>mes</caption>
-          </li>;
-        })}
-      </ul> */}
+      <ul className="resume__chart">
+        {columns.map((column) => (
+          <li className="tooltip">
+            <span
+              className="tooltiptext"
+              style={{ bottom: `${column.height + 28}px` }}
+            >
+              {column.amount}
+            </span>
+            <div
+              className={`column column--${column.amount > 50 && "major"}`}
+              style={{
+                height: `${column.height}px`,
+              }}
+            />
+            <p>{column.day}</p>
+          </li>
+        ))}
+      </ul>
       <hr color="#f8e9d" />
-      <div className="chart__total-month">
+      <div className="resume__total-month">
         <div className="total">
           <h5>Total this month</h5>
           <h1>$478.33</h1>
