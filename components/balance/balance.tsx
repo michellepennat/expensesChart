fb13b541ebfb252900a7e450dd5a6a72505f1620
Michelle Penna <mp.design.apps@gmail.com>
2023-03-28 11:09:25 -0500

    feat(): add balance

---
+++ components/balance/balance.tsx
@@ -0,0 +1,21 @@
+// export interface ICatCard {
+//   tag: string;
+//   title: string;
+//   body: string;
+//   author: string;
+//   time: string;
+// }
+
+const Balance = () => {
+  return (
+    <section className="balance">
+      <div>
+        <h5>My balance</h5>
+        <h3>$921.48</h3>
+      </div>
+      <img src="/img/balanceSVG.svg" alt="Balance" />
+    </section>
+  );
+};
+
+export default Balance;
